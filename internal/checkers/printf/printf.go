6fca8cf3f93fb292b0f9181ba2cdad9fbadc2a7c
Anton Telyshev <anton.telishev@yandex.ru>
2024-06-08 23:58:12 +0300

    Don't use 1.22 features

--- internal/checkers/printf/printf.go
+++ internal/checkers/printf/printf.go
@@ -215,7 +215,7 @@ func isFormatter(typ types.Type) bool {
 
 // isTypeParam reports whether t is a type parameter (or an alias of one).
 func isTypeParam(t types.Type) bool {
-	_, ok := types.Unalias(t).(*types.TypeParam)
+	_, ok := t.(*types.TypeParam)
 	return ok
 }
 
@@ -224,7 +224,7 @@ func isTypeParam(t types.Type) bool {
 // This function avoids allocating the concatenation of "pkg.Name",
 // which is important for the performance of syntax matching.
 func isNamedType(t types.Type, pkgPath string, names ...string) bool {
-	n, ok := types.Unalias(t).(*types.Named)
+	n, ok := t.(*types.Named)
 	if !ok {
 		return false
 	}
