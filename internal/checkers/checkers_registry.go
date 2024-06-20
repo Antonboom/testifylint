9948083b639900751d086e4b28bba0d40f11a82f
Anton Telyshev <anton.telishev@yandex.ru>
2024-06-20 20:35:13 +0300

    new checker 'regexp'

--- internal/checkers/checkers_registry.go
+++ internal/checkers/checkers_registry.go
@@ -17,6 +17,7 @@ var registry = checkersRegistry{
 	{factory: asCheckerFactory(NewNilCompare), enabledByDefault: true},
 	{factory: asCheckerFactory(NewErrorIsAs), enabledByDefault: true},
 	{factory: asCheckerFactory(NewExpectedActual), enabledByDefault: true},
+	{factory: asCheckerFactory(NewRegexp), enabledByDefault: true},
 	{factory: asCheckerFactory(NewSuiteExtraAssertCall), enabledByDefault: true},
 	{factory: asCheckerFactory(NewSuiteDontUsePkg), enabledByDefault: true},
 	{factory: asCheckerFactory(NewUselessAssert), enabledByDefault: true},
---
+++ internal/checkers/helpers_pkg_func.go
@@ -0,0 +1,27 @@
+package checkers
+
+import (
+	"go/ast"
+
+	"golang.org/x/tools/go/analysis"
+
+	"github.com/Antonboom/testifylint/internal/analysisutil"
+)
+
+func isRegexpMustCompileCall(pass *analysis.Pass, ce *ast.CallExpr) bool {
+	return isPkgFnCall(pass, ce, "regexp", "MustCompile")
+}
+
+func isPkgFnCall(pass *analysis.Pass, ce *ast.CallExpr, pkg, fn string) bool {
+	se, ok := ce.Fun.(*ast.SelectorExpr)
+	if !ok {
+		return false
+	}
+
+	errorsIsObj := analysisutil.ObjectOf(pass.Pkg, pkg, fn)
+	if errorsIsObj == nil {
+		return false
+	}
+
+	return analysisutil.IsObj(pass.TypesInfo, se.Sel, errorsIsObj)
+}
