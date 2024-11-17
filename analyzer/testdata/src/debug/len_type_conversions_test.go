1bf6ee1a6901ba3db216f2528c012d25c2881eba
Anton Telyshev <anton.telishev@yandex.ru>
2024-11-17 14:31:31 +0300

    len: empty: add TestBytesReadability

--- analyzer/testdata/src/debug/len_type_conversions_test.go
+++ analyzer/testdata/src/debug/len_type_conversions_test.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"testing"
 
-	"github.com/ghetzel/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestLenTypeConversions(t *testing.T) {
@@ -17,4 +17,16 @@ func TestLenTypeConversions(t *testing.T) {
 	assert.Len(t, resp, respLen)
 	assert.Len(t, []byte(resp), respLen)
 	assert.Len(t, json.RawMessage(resp), respLen)
+}
+
+func TestBytesReadability(t *testing.T) {
+	resp := []byte(`{"status": 200}`)
+	assert.Len(t, resp, 3)         // "[123 34 115 116 97 116 117 115 34 58 32 50 48 48 125]" should have 3 item(s), but has 15
+	assert.Len(t, string(resp), 3) // "{"status": 200}" should have 3 item(s), but has 15
+
+	var noResp []byte
+	assert.Empty(t, resp)              // Should be empty, but was [123 34 115 116 97 116 117 115 34 58 32 50 48 48 125]
+	assert.Empty(t, string(resp))      // Should be empty, but was {"status": 200}
+	assert.NotEmpty(t, noResp)         // Should NOT be empty, but was []
+	assert.NotEmpty(t, string(noResp)) // Should NOT be empty, but was
 }
